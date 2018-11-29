cb939f46d6b92e3f63127257f843eea703984c2d
Raghavasimhan <aavu@gatech.edu>
2018-11-29 00:34:52 -0500

    Hyperparameter Tuning

--- phase_space_embedding/evalModel.py
+++ phase_space_embedding/evalModel.py
@@ -2,24 +2,20 @@ import glob
 import numpy as np
 from keras.models import load_model
 import timeit
+import util_functions as UF
 
-val_set = []
-item_names = []
-items = glob.glob('validation_set_0/**/*.phasespace')
-for item in items:
-    item_names.append(items)
-    data = np.loadtxt(item)
-    if not np.isnan(data).any():
-        val_set.append(data)
-    else:
-        print("found NaN in", item)
+validation_data_folder = "validation_data"
+ragas = UF.list_ragas(validation_data_folder)
+ragas = np.sort(np.array(ragas))
+print(ragas)
+X_val, y_val = UF.load_data(ragas, validation_data_folder)
 
-model = load_model('bestv2/best_model_40c.h5')
+model = load_model('best/best_model_7c.h5')
 
 
 def time_this():
-    model.predict(np.array(val_set).reshape(-1, 121, 121, 1), batch_size=32, verbose=0)
+    model.predict(np.array(X_val), batch_size=32, verbose=0)
 
 
 number = 100
-print(timeit.timeit(time_this, number=100)*1000/(len(val_set)*number))      # avg 0.21351296351528154 ms
+print(timeit.timeit(time_this, number=100) * 1000 / (len(X_val) * number), "ms")  # avg 0.21351296351528154 ms
